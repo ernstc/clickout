300336c50a0f9389480ce3f99d2e9b816b49c77a
Ernesto Cianciotta <ernesto.cianciotta@gmail.com>
2012-05-12 11:22:52 +0200

    First commit.

---
+++ README.txt
@@ -0,0 +1,17 @@
+jQuery Plugin ClickOut
+-----------------------------
+
+by Ernesto Cianciotta (e.cianciotta@abriom.com)
+
+
+
+Those who know jQuery (www.jquery.com) certainly know and appreciate its qualities. For those who do not know, jQuery is a fast and concise JavaScript library that simplifies HTML document traversing, animating, Ajax interactions and event handling.
+
+About event handling, perhaps the most widley used is the "click" event that fires when you click inside the area of a given DOM element.
+
+Sometimes, however, there is the need to handle the event that triggers when you click outsite the area of the DOM element. While the "onclick" is a native JavaScript event and is supported by jQuery with ".click()" function (see API), there is no native event in JavaScript that triggers when you click outside of an element in the HTML page.
+
+To resolve this problem I wrote the plugin ".clickout()"
+
+
+Full blog post: http://ecianciotta-en.abriom.com/2011/09/jquery-plugin-clickou.html
---
+++ jquery.clickout.js
@@ -0,0 +1,74 @@
+﻿/*
+--------------------------------------------------
+Copyright (c) 2011 Ernesto Cianciotta
+Licensed under the MIT License:
+http://www.opensource.org/licenses/mit-license.php
+--------------------------------------------------
+
+	jQuery plugin clickout
+
+	ver 1.0
+	
+	Bind an event handler to the matched elements, to be executed when the user clicks outside 
+	the elements, or trigger that event on an element.	
+
+*/
+
+(function ($)
+{
+
+	var uuid = 0;
+	var clickoutPrefix = 'clickout';
+
+	$.fn.clickout = function (param)
+	{
+		return this.each(function ()
+		{
+			var $this = $(this);
+
+			var handlerID = $this.data('handlerID');
+			if (!handlerID) handlerID = clickoutPrefix + uuid++;
+
+			if ($.isFunction(param))
+			{
+				$this.addClass(handlerID).data('clickoutFunc', param);
+				$(document).bind('click.' + handlerID, function (e)
+				{
+					var el = $(e.srcElement || e.originalTarget || e.target);
+					var handlerID = e.handleObj.namespace;
+
+					if (!(el.hasClass(handlerID) || el.parents('.' + handlerID).length > 0))
+					{
+						var el = $('.' + handlerID);
+						if (el.length == 0)
+						{
+							$(document).unbind('click.' + handlerID);
+						}
+						else
+						{
+							var func = el.data('clickoutFunc');
+							if ($.isFunction(func))
+							{
+								func.call(el.get(0), e);
+							}
+						}
+					}
+				});
+			}
+			else
+			{
+				if (param === false)
+				{
+					$(document).unbind('click.' + handlerID);
+					$this.data('clickoutFunc', $.noop);
+				}
+				else if (!param == null)
+				{
+					$(document).trigger('click.' + $this.data('handlerID'));
+				}
+			}
+		});
+	}
+		
+	
+})(jQuery);
